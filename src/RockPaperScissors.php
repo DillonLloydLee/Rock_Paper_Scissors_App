705c2083ed23173ce701c8ec68e508471c3a55d3
Dillon Lloyd Lee & Brian Borealis <student@epicodus.com>
2015-08-12 10:36:33 -0700

    dumb first test succeed.

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,7 +3,13 @@
 
         function game($player_one, $player_two) {
 
-            
+            if (($player_one == $player_one) && ($player_two == $player_two)) {
+                return "DRAW!";
+            }
+            else {
+                return "ERR";
+            }
+
         }
     }
 
